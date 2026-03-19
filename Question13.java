83748ecf5bb090582113c3023b6d9e32fdfcea4c
himanshubarde29-gif <himanshubarde888@gmail.com>
2026-03-19 18:33:25 +0530

    Add files via upload

---
+++ Question13.java
@@ -0,0 +1,27 @@
+
+package Mainjava;
+
+import java.util.*;
+public class Question13 {
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        int n = arr.length;
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+
+                        List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+        for (List<Integer> list : set) {
+            System.out.println(list);
+        }
+    }
+}
